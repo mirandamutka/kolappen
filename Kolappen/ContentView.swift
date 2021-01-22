0989465da5cdf4f02a8ac36e8257909f7811ecd5
mirandamutka <michirisu@outlook.com>
2021-01-22 13:55:17 +0100

    Initial Commit

---
+++ Kolappen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kolappen
+//
+//  Created by Miranda Mutka on 2021-01-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
