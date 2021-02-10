4c913d407759f73968f632afa521c6dae3a204ba
Robert Horngren <robert@horngren.net>
2021-02-10 12:02:33 +0100

    Added firebase and turned into project

--- Kolappen/ContentView.swift
+++ Kolappen/ContentView.swift
@@ -6,16 +6,25 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseFirestore
 
 struct ContentView: View {
+    
+    var db = Firestore.firestore()
+    
     var body: some View {
         Text("Hello, kölappisar!")
             .padding()
+            .onAppear() {
+                print("running!")
+                db.collection("test").addDocument(data: ["testRun" : "Kölappen \(Date())"])
+            }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Kolappen' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Kolappen
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+  pod 'FirebaseFirestoreSwift'
+
+  target 'KolappenTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'KolappenUITests' do
+    # Pods for testing
+  end
+
+end
